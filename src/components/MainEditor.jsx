978691636139ca9286cdc70c1d462d74af9d5b2c
naveed0000 <sknaveed99206@gmail.com>
2025-11-11 13:56:44 +0530

    fixed env

--- src/components/MainEditor.jsx
+++ src/components/MainEditor.jsx
@@ -1,67 +1,58 @@
 // src/MainEditor.jsx
-import React, { useState, useCallback } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 import {
-    ClassicEditor,
-    Essentials,
-    Paragraph,
-    Bold,
-    // Italic,
-    // GeneralHtmlSupport,
-    // List,
-    // ListUI,
-    // ListProperties,
-} from 'ckeditor5';
+  ClassicEditor,
+  Essentials,
+  Paragraph,
+  Bold,
+  // Italic,
+  // GeneralHtmlSupport,
+  // List,
+  // ListUI,
+  // ListProperties,
+} from "ckeditor5";
 
-import EquationPlugin from './EquationPlugin';
-import KaTeXRenderPlugin from './KaTeXRenderPlugin';
-import EqEditorModal from './EqEditorModal';
-import 'ckeditor5/ckeditor5.css';
-import MathType from '@wiris/mathtype-ckeditor5/dist/index.js';
+import EquationPlugin from "./EquationPlugin";
+import KaTeXRenderPlugin from "./KaTeXRenderPlugin";
+import EqEditorModal from "./EqEditorModal";
+import "ckeditor5/ckeditor5.css";
+import MathType from "@wiris/mathtype-ckeditor5/dist/index.js";
 
 function MainEditor() {
+  const [editorData, setEditorData] = useState(
+    "<p>Type text and insert equations inline or as blocks.</p>"
+  );
+  console.log("editorData: ", editorData);
 
-    const [editorData, setEditorData] = useState('<p>Type text and insert equations inline or as blocks.</p>');
-    console.log('editorData: ', editorData);
+  return (
+    <>
+      <CKEditor
+        editor={ClassicEditor}
+        className="my-editor"
+        config={{
+          // Your license key here
+          licenseKey: import.meta.env.VITE_CKEDITOR,
+          plugins: [Essentials, Paragraph, Bold, MathType],
+          toolbar: [
+            "undo",
+            "redo",
+            "|",
+            "bold",
+            "MathType",
+            // 'ChemType',
+          ],
 
-
-    return (
-        <>
-            <CKEditor
-                editor={ClassicEditor}
-                className="my-editor"
-                config={{
-                    // Your license key here
-                    licenseKey:
-                        'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3OTI4ODYzOTksImp0aSI6ImY5YTk2MjNlLWY1ZTYtNDFjMy1hM2E3LWVlNDViNzFiNTNjYiIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJyZW1vdmVGZWF0dXJlcyI6WyJQQiIsIlJGIiwiU0NIIiwiVENQIiwiVEwiLCJUQ1IiLCJJUiIsIlNVQSIsIkI2NEEiLCJMUCIsIkhFIiwiUkVEIiwiUEZPIiwiV0MiLCJGQVIiLCJCS00iLCJGUEgiLCJNUkUiXSwidmMiOiIwZDRjZGIxZiJ9.GHN-RNSERoAQtrrnPex160lxVDMsbmQ7NtQ_hrn_VLpISdp2U85FRKPAg81sPOBIa2B6yKPPe-O9dHwDRD91HQ',
-                    plugins: [
-                        Essentials,
-                        Paragraph,
-                        Bold,
-                        MathType,
-                    ],
-                    toolbar: [
-                        'undo',
-                        'redo',
-                        '|',
-                        'bold',
-                        'MathType',
-                        // 'ChemType',
-                    ],
-
-                    data: '<p>Type text and insert equations inline or as blocks.</p>',
-                }}
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    setEditorData(data);
-                    console.log('Editor data:', data);
-                }}
-
-            />
-
-
-        </>
-    );
+          data: "<p>Type text and insert equations inline or as blocks.</p>",
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setEditorData(data);
+          console.log("Editor data:", data);
+        }}
+      />
+    </>
+  );
 }
 
 export default MainEditor;
